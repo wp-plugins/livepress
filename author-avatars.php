2f983f7f22b56cd45b4914e4795e6038c49a2707
bforchhammer <bforchhammer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-08 13:29:17 +0000

    Updated plugin url in header
    git-svn-id: https://plugins.svn.wordpress.org/author-avatars/trunk@72811 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- author-avatars.php
+++ author-avatars.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Author avatars
-Plugin URI:
+Plugin URI: http://wordpress.org/extend/plugins/author-avatars/
 Description: Adds a <a href="widgets.php">widget</a> that shows avatars of the blog users so that people can see what you look like. This uses the standard avatar to retrieve the images. You can size, label, filter by roles/usernames, order...
 Version: 0.1
 Author: <a href="http://mind2.de">Benedikt Forchhammer</a>, Idea: <a href="http://bearne.com">Paul Bearne</a>
