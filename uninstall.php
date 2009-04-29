57800741a542183a817789a3e9aba08dd5ee3351
bforchhammer <bforchhammer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-29 13:06:19 +0000

    Removed invalid characters (fixes uninstall behaviour).
    git-svn-id: https://plugins.svn.wordpress.org/author-avatars/trunk@114304 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,6 @@
 <?php
 // exit if we are not uninstalling the plugin...
-if( !defined( ‘ABSPATH’) && !defined(’WP_UNINSTALL_PLUGIN’) ) exit();
+if( !defined( 'ABSPATH' ) && !defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 
 // delete plugin options
 delete_option('author_avatars_version');
