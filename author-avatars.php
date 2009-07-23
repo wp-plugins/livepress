b6ea1979b28ae9c4a4be098ff5ee501d551a4216
bforchhammer <bforchhammer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-23 07:59:47 +0000

    Updated PluginURL to new website
    git-svn-id: https://plugins.svn.wordpress.org/author-avatars/trunk@138218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- author-avatars.php
+++ author-avatars.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Author Avatars List
-Plugin URI: http://wordpress.org/extend/plugins/author-avatars/
+Plugin URI: http://authoravatars.wordpress.com/
 Description: Display lists of user avatars using <a href="widgets.php">widgets</a> or <a href="http://codex.wordpress.org/User:Bforchhammer/Author_Avatars_ShortCode_Documentation">shortcodes</a>.
-Version: 0.7.4
+Version: 0.7.5-dev
 Author: <a href="http://mind2.de">Benedikt Forchhammer</a>, Idea: <a href="http://bearne.com">Paul Bearne</a>
 Text Domain: author-avatars
 Domain Path: /translations/
