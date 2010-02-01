45f460729a615fc99622d4747ee0df8de593fc5c
bforchhammer <bforchhammer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-01 11:19:03 +0000

    Fixed formatting (indentation)
    git-svn-id: https://plugins.svn.wordpress.org/author-avatars/trunk@200424 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/UserList.class.php
+++ lib/UserList.class.php
@@ -218,16 +218,16 @@ class UserList {
 
 		$avatar = get_avatar($user->user_id, $avatar_size);
 
-                /* Strip all existing links (a tags) from the get_avatar() code to
-                 * remove e.g. the link which is added by the add-local-avatar plugin
-                 * @see http://wordpress.org/support/topic/309878 */
-                if (!empty($link)) {
-                    $avatar = preg_replace('@<\s*\/?\s*[aA]\s*.*?>@', '', $avatar);
-                }
-                /* strip alt and title parameter */
+		/* Strip all existing links (a tags) from the get_avatar() code to
+		* remove e.g. the link which is added by the add-local-avatar plugin
+		* @see http://wordpress.org/support/topic/309878 */
+		if (!empty($link)) {
+			$avatar = preg_replace('@<\s*\/?\s*[aA]\s*.*?>@', '', $avatar);
+		}
+		/* strip alt and title parameter */
 		$avatar = preg_replace('@alt=["\'][\w]*["\'] ?@', '', $avatar);
-                $avatar = preg_replace('@title=["\'][\w]*["\'] ?@', '', $avatar);
-                /* insert alt and title parameters */
+		$avatar = preg_replace('@title=["\'][\w]*["\'] ?@', '', $avatar);
+		/* insert alt and title parameters */
 		$avatar = preg_replace('@ ?\/>@', ' alt="'.$name.'" title="'.$name.'" />', $avatar);
 
 		$html = '';
