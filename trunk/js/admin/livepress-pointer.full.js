0f735eea703647d525757fa7a967289da589daff
ericmann <ericmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-31 20:18:26 +0000

    Add version 1.0.
    git-svn-id: https://plugins.svn.wordpress.org/livepress@748914 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/js/admin/livepress-pointer.full.js
@@ -0,0 +1,37 @@
+/*! LivePress -v0.7.3 - 2013-07-16
+ * http://livepress.com/
+ * Copyright (c) 2013 LivePress, Inc.
+ */
+( function( window, $, undefined ) {
+	var CORE = window.livepress_pointer;
+
+	function Pointer( element ) {
+		var SELF = this,
+			$element = $( element );
+
+		SELF.close = function () {
+			$.post( CORE.ajaxurl, {
+				pointer: 'livepress_pointer',
+				action:  'dismiss-wp-pointer'
+			} );
+		};
+
+		SELF.open = function() {
+			$element.pointer( options ).pointer( 'open' );
+		};
+
+		var options = {
+			'content': CORE.content,
+			'position': {
+				'edge': 'top',
+				'align': 'left'
+			},
+			'close': SELF.close
+		};
+	}
+
+	$( window ).on( 'livepress.blogging-tools-loaded', function() {
+		var pointer = CORE.pointer = new Pointer( '#blogging-tools-link-wrap' );
+		pointer.open();
+	} );
+}( this, jQuery ) );
