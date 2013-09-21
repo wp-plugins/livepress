cdafc37293e1f52e25189ca31c35a39694102fe2
adamsilverstein <adamsilverstein@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-21 06:25:10 +0000

    1.0.4 release
    git-svn-id: https://plugins.svn.wordpress.org/livepress@776264 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.4/livepress.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: LivePress
+Plugin URI:  http://www.livepress.com
+Description: Enables WordPress to post live using the LivePress API.
+Version:     1.0.3
+Author:      LivePress Inc.
+Author URI:  http://www.livepress.com
+*/
+
+define( 'LP_PLUGIN_VERSION', '1.0.3' ); // Please sync this with Version in header
+define( 'PLUGIN_NAME', 'livepress' );
+
+define( 'LP_STORE_URL', 'http://livepress.com/' ); // Store powered by Easy Digital Downloads
+define( 'LP_ITEM_NAME', 'LivePress' );
+
+function livepress_load_plugin_textdomain() {
+	load_plugin_textdomain( 'livepress', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'init', 'livepress_load_plugin_textdomain' );
+
+require 'php/livepress-boot.php';
+
+?>
